c98649024ca5bc36b45ce00b46f44ccc166b2549
Anita <yadavanita632@gmail.com>
2022-03-01 11:41:29 +0530

    second project

---
+++ src/pack/cls.java
@@ -0,0 +1,20 @@
+package pack;
+
+public class cls{
+
+	
+		
+		
+		public static void m1()
+		{
+			System.out.println("mera name moommoom:m1");
+			
+		}
+		public static void m2()
+		{
+			System.out.println("mera naname miss jalebi:m2");
+		}
+
+	}
+
+	
---
+++ src/pack/cls2.java
@@ -0,0 +1,13 @@
+package pack;
+
+public class cls2 {
+	public static void main (String[]args)
+	{
+		System.out.print("");
+		cls s4 = new cls();
+		s4.m1();
+		s4.m2();
+		
+	}
+
+}
